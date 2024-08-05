ba2e11b6b3f6b8be32ae78040304ae35e9a11df2
Serhii Cherkasov <yourdev@ukrnet.com>
2024-08-05 21:12:39 +0300

    temporary fix for zod types

--- src/lib/database-actions.ts
+++ src/lib/database-actions.ts
@@ -6,30 +6,7 @@ import {redirect} from "next/navigation";
 import { FormSchema} from "@/src/lib/schemas";
 import {Writeable, ZodEnum, ZodNumber, ZodString} from "zod";
 
-export async function createInvoice(data: {
-  date: string;
-  amount: Omit<{
-    date: ZodString;
-    amount: ZodNumber;
-    customerId: ZodString;
-    id: ZodString;
-    status: ZodEnum<Writeable<[string, string]>>
-  }, "date" | "id">["amount"]["_output"];
-  customerId: Omit<{
-    date: ZodString;
-    amount: ZodNumber;
-    customerId: ZodString;
-    id: ZodString;
-    status: ZodEnum<Writeable<[string, string]>>
-  }, "date" | "id">["customerId"]["_output"];
-  status: Omit<{
-    date: ZodString;
-    amount: ZodNumber;
-    customerId: ZodString;
-    id: ZodString;
-    status: ZodEnum<Writeable<[string, string]>>
-  }, "date" | "id">["status"]["_output"]
-}) {
+export async function createInvoice(data: typeof FormSchema | any) {
   const { customerId, amount, status, date } = data
 
   try {
@@ -49,30 +26,7 @@ export async function createInvoice(data: {
 }
 
 
-export async function updateInvoice(data: {
-  amount: Omit<{
-    date: ZodString;
-    amount: ZodNumber;
-    customerId: ZodString;
-    id: ZodString;
-    status: ZodEnum<Writeable<[string, string]>>
-  }, "date" | "id">["amount"]["_output"];
-  customerId: Omit<{
-    date: ZodString;
-    amount: ZodNumber;
-    customerId: ZodString;
-    id: ZodString;
-    status: ZodEnum<Writeable<[string, string]>>
-  }, "date" | "id">["customerId"]["_output"];
-  id: File | string;
-  status: Omit<{
-    date: ZodString;
-    amount: ZodNumber;
-    customerId: ZodString;
-    id: ZodString;
-    status: ZodEnum<Writeable<[string, string]>>
-  }, "date" | "id">["status"]["_output"]
-}) {
+export async function updateInvoice(data: typeof FormSchema | any) {
   const { customerId, amount, status, id } = data
 
   try {
